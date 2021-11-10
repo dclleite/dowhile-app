8a2fabafeaf8998b0858f7c1515b97ef4fe32755
dclleite <eduappsplay@gmail.com>
2021-11-10 00:04:32 -0300

    feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import {
-  View, 
-  Text,
-  TouchableOpacity
-} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg'
+import { UserPhoto } from '../UserPhoto';
 
 export function Header(){
   return (
@@ -16,9 +13,15 @@ export function Header(){
 
       <LogoSvg />
 
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+      <View style={styles.logoutButton} >
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        
+        <UserPhoto imageUri="https://github.com/dclleite.png" />
+      </View>
+
+      
 
     </View>
   );
