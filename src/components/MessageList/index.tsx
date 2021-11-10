9314ef6607a2e722289d6427e55b87e760a99bd1
dclleite <eduappsplay@gmail.com>
2021-11-10 00:23:06 -0300

    feat: MessageList component

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { ScrollView } from 'react-native';
+import { Message } from '../Message';
+
+import { styles } from './styles';
+
+export function MessageList(){
+
+  const message = {
+    id: "320",
+    text: "message text",
+    user: {
+      name: "Eduardo Leite",
+      avatar_url: "",
+    }
+  }
+
+  return (
+    <ScrollView 
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps='never'
+    >
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+
+    </ScrollView>
+  );
+}
